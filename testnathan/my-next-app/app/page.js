1bef806254d5b855e607e13a59eac0f59eacbc32
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2025-01-10 13:25:52 +0530

    test
    test2

---
+++ testnathan/my-next-app/app/page.js
@@ -0,0 +1,9 @@
+import Registration from "./register/page";
+
+export default function Home() {
+  return (
+    <div>
+      <Registration />
+    </div>
+  );
+}
---
+++ testnathan/my-next-app/lib/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const connectToDatabase = async () => {
+  try {
+  } catch (error) {
+    console.log("Error connecting to database", error);
+  }
+};
